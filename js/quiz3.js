afac653e804cb54517861742946307f2e67cdb20
Suwantha <suwanthagg@gmail.com>
2020-09-16 15:23:55 +0700

    Create quiz3.js

---
+++ js/quiz3.js
@@ -0,0 +1,15 @@
+score = 90
+if ( score >=81 && score <=100 ){
+    return "A"
+
+}else if ( score >= 71 && score <= 80){
+    return "B"
+
+}else if( score >=61 && score <= 70){
+    return "C"
+}else if ( socre >=51 && score <=60) {
+    return "D"
+}else{
+    score <= 49
+        return "F"
+}
