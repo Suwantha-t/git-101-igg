be735e6ee5564b4052b00eb5676ba4d5c182ccaf
Suwantha <suwanthagg@gmail.com>
2020-09-16 13:38:28 +0700

    creat example

---
+++ js/example.js
@@ -0,0 +1,24 @@
+
+const PI = 3.14
+
+// console.log("PI: ", PI)
+
+let name = "John"
+name = "Joke"
+
+let test = "Hello"
+name = `${test} B`
+
+let arr = ['apple', 'banana', 'kiwi']
+console.log("name: ", arr[1])
+
+let person = {
+    name: 'JOKE',
+    age: 15,
+    gender: {
+        name: 'male'
+    },
+    arr
+}
+
+console.log(person)
