522c78b5d81eb045130143138d4ea06b3f860558
Suwantha <suwanthagg@gmail.com>
2020-09-16 14:06:39 +0700

    create function

---
+++ js/function.js
@@ -0,0 +1,20 @@
+// function add(a, b) {
+//     return a + b
+// }
+
+// function mul(a, b){
+//     return a * b
+// }
+
+// function searchCustomer(customerName){
+//     //find customer
+// }
+// const add = (a, b) => {
+//     return a + b 
+// }
+
+const add = (a, b) => {
+    return a + b
+}
+
+console.log(add(2, 3))
