a8211a63422e0e14cfa385892d15e92d541838a3
Suwantha <suwanthagg@gmail.com>
2020-09-16 15:48:01 +0700

    create synchronus

---
+++ js/synchronus.js
@@ -0,0 +1,14 @@
+const { resolve } = require("path")
+const { reject } = require("q")
+
+console.log("1")
+console.log("2")
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(console.log("3"))
+    }, 1000);
+    
+}).then(() => {
+    console.log("4")
+    console.log("5")
+})
