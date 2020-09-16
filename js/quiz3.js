7a91feb2db0edddd0e55a68b1f8c5a6da64244a6
Suwantha <suwanthagg@gmail.com>
2020-09-16 15:40:58 +0700

    create quiz4

--- js/quiz3.js
+++ js/quiz3.js
@@ -1,15 +1,23 @@
-score = 90
-if ( score >=81 && score <=100 ){
-    return "A"
+let point = 0
 
-}else if ( score >= 71 && score <= 80){
-    return "B"
+const calculateGrade = (point) => {
+    let grade = null
+    if ( point >=81 && point <=100 ){
+        grade =  "A"
 
-}else if( score >=61 && score <= 70){
-    return "C"
-}else if ( socre >=51 && score <=60) {
-    return "D"
-}else{
-    score <= 49
-        return "F"
+    }else if ( point >= 71 && point <= 80){
+        grade = "B"
+
+    }else if( point >=61 && point <= 70){
+        grade = "C"
+    }else if ( point >=51 && point <=60) {
+        grade = "D"
+    }else if ( point >=51 && point <=60) {
+        grade = "F"
+    }else{
+        grade = "Point was invaliable"
+    }
+    return grade
+
 }
+console.log(calculateGrade(70))
---
+++ js/quiz4.js
@@ -0,0 +1,14 @@
+//length()
+var text = "Hello my name is Joke"
+var sln = text.length
+console.log(sln)
+
+// indexOf()
+var txt = "Hello my name is Joke nice to meet you"
+var pos = txt.indexOf('Joke');
+console.log(pos)
+
+// search()
+var str = "Well open up your mind and see like me"
+var pos = str.search("open")
+console.log(pos)
