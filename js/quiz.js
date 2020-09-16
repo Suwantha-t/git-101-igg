6f82504f0d2ad0acd4daeea4bb7e727a4a9aad4e
Suwantha <suwanthagg@gmail.com>
2020-09-16 13:49:46 +0700

    create quiz.js

---
+++ js/quiz.js
@@ -0,0 +1,10 @@
+let profile = {
+    first_name: 'Suwantha',
+    last_name: 'Thongpanit',
+    age: 22,
+    nickname: 'Gib',
+    hobbie: ' Go to cinema ',
+    phone_number: '088-142-XXXX',
+
+}
+console.log(profile)
