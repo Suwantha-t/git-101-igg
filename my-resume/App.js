f702dcf6904a24eb3a1585f02ac17c9d293ac8a6
Suwantha <suwanthagg@gmail.com>
2020-09-18 12:10:19 +0700

    create screen

--- my-resume/App.js
+++ my-resume/App.js
@@ -2,31 +2,25 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import ResumeForm from './screen/ResumeForm'
 
-function HomeScreen(props) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Go to Detail Screen" onPress={() => props.navigation.push('Detail')}></Button>
-    </View>
-  );
-}
-function DetailScreen(props) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Detail Screen</Text>
-      {/* <Button title="Go to Home Screen" onPress={() => props.navigation.push('Home')}></Button> */}
-    </View>
-  );
-}
+// function DetailScreen(props) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Detail Screen</Text>
+//       {/* <Button title="Go to Home Screen" onPress={() => props.navigation.push('Home')}></Button> */}
+//     </View>
+//   );
+// }
 
 const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" options={{title: 'Home page'}} component={HomeScreen} />
-        <Stack.Screen name="Detail" options={{title: 'Detail page'}} component={DetailScreen} />
+        {/* add */}
+        <Stack.Screen name="ResumeForm" options={{title: 'Resume Form'}} component={ResumeForm} />
+        {/* <Stack.Screen name="Detail" options={{title: 'Detail page'}} component={DetailScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
